09d1d0e71f84dbf7b510d09cead0b71ee1706636
Thu Pham <ptthu@gmx.de>
2024-05-22 16:22:39 +0200

    cards and pictures done

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ ship = Ship.new(
 ship.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 ship.save
 
-file = URI.open("https://preview.redd.it/qyi98jfbwqt61.jpg?width=640&crop=smart&auto=webp&s=7d933e4e2679a7d7790fe7a944077df8f46919d3")
+file = URI.open("https://images.unsplash.com/photo-1568347877321-f8935c7dc5a3?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 ship = Ship.new(
   name: 'Queen Annes Revenge',
   description: 'The ship of the infamous pirate Blackbeard',
@@ -42,7 +42,7 @@ ship = Ship.new(
 ship.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 ship.save
 
-file = URI.open("https://preview.redd.it/qyi98jfbwqt61.jpg?width=640&crop=smart&auto=webp&s=7d933e4e2679a7d7790fe7a944077df8f46919d3")
+file = URI.open("https://www.merkur.de/assets/images/28/580/28580509-vor-110-jahren-sank-die-titanic-2uea.jpg")
 ship = Ship.new(
   name: 'Titanic',
   description: 'A British passenger liner that sank in the North Atlantic Ocean',
@@ -54,7 +54,7 @@ ship = Ship.new(
 ship.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 ship.save
 
-file = URI.open("https://preview.redd.it/qyi98jfbwqt61.jpg?width=640&crop=smart&auto=webp&s=7d933e4e2679a7d7790fe7a944077df8f46919d3")
+file = URI.open("https://images.nightcafe.studio/jobs/Y5uL5Hx5jue6hOwCb7ao/Y5uL5Hx5jue6hOwCb7ao--1--yv392_11.3636x.jpg?tr=w-1600,c-at_max")
 ship = Ship.new(
   name: 'Black Pearl',
   description: 'A legendary pirate ship known for its speed and captained by Jack Sparrow',
