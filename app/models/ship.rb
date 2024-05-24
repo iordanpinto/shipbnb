334bab729452e44426aa5f080b5a27481b12bbea
Iordan Pinto <pintoiordan@gmail.com>
2024-05-24 12:59:07 +0200

    geocoding map installed on index

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,4 +3,7 @@ class Ship < ApplicationRecord
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
   # validates :image_url, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, allow_blank: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
