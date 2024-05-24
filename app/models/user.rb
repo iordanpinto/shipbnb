a723f2b9368e8f295c2b49955836f6cb589834ad
Iordan Pinto <pintoiordan@gmail.com>
2024-05-24 16:33:34 +0200

    dependecies on user updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :ships, dependent: :destroy
 end
